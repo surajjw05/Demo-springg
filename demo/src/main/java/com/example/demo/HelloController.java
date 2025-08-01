1ca61691b781de9433218b0549980a82088cca78
Suraj Walve <surajwalve39@gmail.com>
2025-08-01 11:24:31 +0530

    Create HelloController.java

---
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from Boot!";
+    }
+
+    @GetMapping("/status")
+    public String getStatus() {
+        return "Application is running 01.";
+    }
+}
